addd87aa57f33c88dcc20a249eef61ee62f00230
Olga Yakimovich <olgayakimovich87@gmail.com>
2019-09-03 15:59:13 +0300

    add passport

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: 'http://localhost:5000/auth/google/callback',
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
+    },
+  ),
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
