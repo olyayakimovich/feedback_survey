d12d5ac9c5ea2e252f78761cacf166c88bf0058c
Olga Yakimovich <olgayakimovich87@gmail.com>
2019-09-09 10:41:06 +0300

    add react app

---
+++ reactClient/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+        <a
+          className="App-link"
+          href="/auth/google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sign In With Google
+        </a>
+    </div>
+)
+
+export default App;
