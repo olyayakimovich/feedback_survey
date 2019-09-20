15f3d802b6b9a39bac61e718df75bdfe941f5e0e
Olga Yakimovich <olgayakimovich87@gmail.com>
2019-09-20 16:23:23 +0300

    add stripe credentials

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  callbackURL: 'https://tranquil-anchorage-44272.herokuapp.com/auth/google/callback'
-}
+  callbackURL:
+    'https://tranquil-anchorage-44272.herokuapp.com/auth/google/callback',
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+};
